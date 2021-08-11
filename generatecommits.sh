7df49101eb9612eeffb5b092434fce3cbbbfddbe
carlofholy <carl.smestad@gmail.com>
2021-08-11 17:51:59 +0200

    feat: new

--- generatecommits.sh
+++ generatecommits.sh
@@ -4,6 +4,8 @@ max=$1
 GIT='git --git-dir='$PWD'/.git'
 for (( i=1; i <= $max; ++i ))
 do
+    echo "$i.txt" >> "$i.txt"
+    $GIT add .
     $GIT commit -m "feat: commit number $i" --allow-empty
 done
 
